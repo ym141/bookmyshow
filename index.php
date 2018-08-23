108e19980e3bb6d6f517bc7e2aedde042fc9e5dc
ym141 <42654371+ym141@users.noreply.github.com>
2018-08-23 12:09:36 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+?>
+
+<html>
+<head>
+<link href="home.css" rel="stylesheet" type="text/css"/>
+<script src="js/index.js"></script>
+</head>
+<body onload="moveSlider()" style="background-color:red;">
+
+<div id="outer">
+<div id="header"><div id="h1"><image src="images/logo.jpg"/></div><div id="h2"><marquee><h1><span style="color:blue;">B<span><span style="color:red;">O</span><span style="color:yellow;">O</span><span style="color:purple">K</span>&nbsp;&nbsp;<span style="color:green">M</span><span style="color:blue">Y</span>
+&nbsp;&nbsp;<span style="color:red">T</span><span style="color:purple">I</span><span style="color:red">C</span><span style="color:green">K</span><span style="color:blue">E</span><span style="color:yellow">T</h1>
+</marquee></div>
+</div>
+<div id="l">
+
+<div id="menu">
+<div id="1">
+<ul>
+		<li style="background-color:#fcc88e;"><a href="#"><a href="index.php">Home</a></li>
+		<li><a href="login.php">Display Moovies</a></li>
+		<li><a href="search.php">Search</a></li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<span id="navr"><a href="login.php">Sign In</a></span>
+	</ul></div>	 </div>
+	</div>	 
+<div id="l1">
+<img id="imslide" src="images/5.jpg" width="700px" height="400"/></div>
+<div id="l3">
+<div id="l31"><center><h2><span style="color:blue">All language</span></h2></center></div>
+<div id="l32"><center><h2><span style="color:blue">Cinema</span></h2></center></div>
+</div>
+<div id="l4">
+<div id="l41"><a href="login.php"><center><marquee><h2><span style="color:red">N</span><span style="color:#004080">E</span><span style="color:yellow">W</span></h2></div>
+</a></marquee></center></div></div>
+<div id="l5">
+<div id="l51"><image src="images/p4.jpg"/></div>
+<div id="l52"><image src="images/p3.jpg"/></div>
+<div id="l53"><image src="images/p5.jpg"/></div>
+<div id="l54"><image src="images/p8.jpg"/></div>
+</div>
+<div id="l6">
+<div id ="l61"><a href="login.php"><center><marquee><h2><span style="color:red">B</span><span style="color:#800080">O</span><span style="color:#ff0080">O</span><span style="color:yellow">K</span></h2></div>
+</a></marquee></center></div></div>
+<div id="r">
+<div id="r1">
+<div id="r11"><image src="images/b1.jpg"/></div>
+<div id="r12"><image src="images/s6.jpg"/></div>
+<div id="r13"><image src="images/b10.jpg"/></div>
+<div id="r14"><image src="images/a4.jpg"/></div>
+<div id="r15"><image src="images/b12.jpg"/></div>
+<div id="r16"></div>
+</div>
+<div id="r2">
+<div id="r21"><image src="images/b6.jpg"/></div>
+<div id="r22"><image src="images/b8.jpg"/></div>
+<div id="r23"><span style="color:white"><center><h1>we are looking for our<br/>"HEROES"</h1></center></span></div>
+</div>
+<div id="r3">
+<div id="r31"><image src="images/b4.jpg"/></div>
+<div id="r32"><image src="images/b3.jpg"/></div>
+<div id="r33"></div>
+</div>
+<div id="r4">
+<div id="r41"><image src="images/s3.jpg"/></div>
+<div id="r42"><image src="images/s5.jpg"/></div>
+<div id="r43"><image src="images/s1.jpg"/></div>
+<div id="r44"><image src="images/s2.jpg"/></div>
+<div id="r45"><image src="images/b4.jpg"/></div>
+</div>
+<div id="footer">
+	<div id="content1">
+		<h4>Customer Care</h4>
+		<table>
+		<tr>
+		<td>Helpline</td>
+		<td>8090805556</td>
+		</tr>
+		<tr>
+		<td>Whatsapp</td>
+		<td>8090805556</td>
+		</tr>
+		<tr>
+		<td>SMS</td>
+		<td>8090805556</td>
+		</tr>
+		</table>
+		</div>
+		<div id="content2">
+		<h4><a href="contactus.php">Contact Us</a></h4>
+		<table>
+		<tr>
+		<td>Name</td>
+		<td>Book My Ticket</td>
+		</tr>
+		<tr>
+		<td>Email</td>
+		<td>bookmyticket@gmail.com</td>
+		</tr>
+		<tr>
+		<td>Mobile</td>
+		<td>+91 8090805556</td>
+		</tr>
+		<tr>
+		<td>Address</td>
+		<td>Book My Ticket<br/>
+		Lucknow
+		</td>
+		</tr>
+		</table>
+		</div>
+		<div id="content3"><h4>About Us</h4>
+		Book my Ticket is designed by Soft Pro's Students.
+		 Softpro Learning Center is unique training center that provides such trainers which are excellent. 
+		</div>
+	</div>
+	</div>
+	<div>
+	
+	
+
+
+
